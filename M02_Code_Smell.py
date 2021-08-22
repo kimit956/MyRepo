1588e7810d2f1a8c8fdf25b6a808cc365fe4730c
kimit956 <kianna.mitchell2001@gmail.com>
2021-08-22 12:14:01 -0400

    Adding Module 2 debugging program

---
+++ M02_Code_Smell.py
@@ -0,0 +1,5 @@
+uncle_sam = 1.08
+pay_the_server = 1.15
+food_stuff = float(input("How much was your meal? "))
+pocket_book_pain = uncle_sam * food_stuff * pay_the_server
+print("You need to leave " + str(pocket_book_pain) + " on the table to cover tax and tip.")
